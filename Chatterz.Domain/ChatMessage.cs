5b7621e07056db745b5d8480bbf384c2a56ca7f8
Maurits <mauritsseelen@hotmail.com>
2023-01-12 12:39:09 +0100

    added username to chatmessage

--- Chatterz.Domain/ChatMessage.cs
+++ Chatterz.Domain/ChatMessage.cs
@@ -6,5 +6,6 @@
         public string Text { get; set; }
         public string ConnectionId { get; set; }
         public DateTime DateTime { get; set; }
+        public string UserName { get; set; }
     }
 }
