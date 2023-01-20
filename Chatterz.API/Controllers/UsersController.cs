25d504d345af0a65eb9aa76bd5aedcb84ed2ef75
Maurits <mauritsseelen@hotmail.com>
2023-01-20 16:44:46 +0100

    show username change in chatroom

--- Chatterz.API/Controllers/UsersController.cs
+++ Chatterz.API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 using Chatterz.API.InMemoryDb;
 using Chatterz.Domain;
 using Chatterz.Domain.DTO;
+using Chatterz.HUBS;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Chatterz.API.Controllers
 {
@@ -9,17 +11,21 @@ namespace Chatterz.API.Controllers
     public class UsersController : ControllerBase
     {
         private IUsersDb _usersDb;
-
-        public UsersController(IUsersDb usersDb)
+        private IHubContext<ChatHub> _hubContext;
+        
+        public UsersController(IUsersDb usersDb, IHubContext<ChatHub> hubContext)
         {
             _usersDb = usersDb;
+            _hubContext = hubContext;
         }
 
         [HttpPost]
         [Route("api/users/change_username")]
-        public ActionResult ChangeUsername(ChangeUsernameDto dto)
+        public async Task<ActionResult> ChangeUsername(ChangeUsernameDto dto)
         {
             _usersDb.ChangeUsername(dto.NewUsername, dto.UserId);
+            await _hubContext.Clients.Group(dto.ChatroomId).SendAsync("UsernameUpdated", dto);
+
             return Ok();
         }
 
--- Chatterz.Domain/DTO/ChangeUsernameDto.cs
+++ Chatterz.Domain/DTO/ChangeUsernameDto.cs
@@ -2,7 +2,9 @@ namespace Chatterz.Domain.DTO
 {
     public class ChangeUsernameDto
     {
+        public string OldUsername { get; set; }
         public string NewUsername { get; set; }
         public string UserId { get; set; }
+        public string ChatroomId { get; set; }
     }
 }
