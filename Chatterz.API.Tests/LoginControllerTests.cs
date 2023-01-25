8729c23aef853cd6653241bd5dbd85c4050d73a2
Maurits <mauritsseelen@hotmail.com>
2023-01-25 23:22:03 +0100

    unit testttttttts

--- Chatterz.API.Tests/LoginControllerTests.cs
+++ Chatterz.API.Tests/LoginControllerTests.cs
@@ -2,38 +2,54 @@ using Chatterz.API.Controllers;
 using Chatterz.Domain.Models;
 using Chatterz.Domain.DTO;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Chatterz.Services.Interfaces;
 
 namespace Chatterz.API.Tests
 {
     [TestClass]
     public class LoginControllerTests
     {
+        private readonly Mock<IUserService> _userServiceMock;
         private readonly LoginController _loginController;
 
         public LoginControllerTests()
         {
-            // _loginController = new LoginController(_usersDb);
+            _userServiceMock = new Mock<IUserService>();
+            _loginController = new LoginController(_userServiceMock.Object);
         }
 
         [TestMethod]
-        public void Test_Login_Returns_LoggedIn_Users()
+        public async Task Test_Login_Returns_LoggedIn_Users()
         {
-            // TODO: mock service and inject in controller
-            // check if methods were called etc
-
             // arrange
-            // var userLoginInfo = new UserLoginDto()
-            // {
-            //     UserName = "john",
-            //     Password = "123"
-            // };
+            var userLoginInfo = new UserLoginDto()
+            {
+                UserName = "john",
+                Password = "123"
+            };
 
-            // // act
-            // var result = _loginController.Login(userLoginInfo);
+            // act
+            var result = await _loginController.Login(userLoginInfo);
 
-            // // asert
-            // Assert.IsNotNull(result);
-            // Assert.IsInstanceOfType(result, typeof(ActionResult<User>));
+            // asert
+            _userServiceMock.Verify(s => s.Login(userLoginInfo.UserName, userLoginInfo.Password), Times.Once);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ActionResult<User>));
+        }
+
+        [TestMethod]
+        public async Task Test_Logout_LogsUserOut()
+        {
+            // arrange
+            var id = 1;
+
+            // act
+            var res = await _loginController.Logout(id);
+
+            // assert
+            _userServiceMock.Verify(s => s.Logout(id), Times.Once);
+            Assert.IsInstanceOfType(res, typeof(OkResult));
         }
     }
 }
