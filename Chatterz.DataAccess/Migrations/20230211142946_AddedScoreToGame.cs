d9fa7f4fc39a4b0487b13cc7b9719793ab449d1a
Maurits Seelen <mauritsseelen@hotmail.com>
2023-02-11 15:30:47 +0100

    added score to game

---
+++ Chatterz.DataAccess/Migrations/20230211142946_AddedScoreToGame.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chatterz.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedScoreToGame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "WordGuessers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "WordGuessers");
+        }
+    }
+}
