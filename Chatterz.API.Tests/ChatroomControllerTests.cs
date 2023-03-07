395d7a5de7e031b61e1c49240bff952e30a2d09e
Maurits Seelen <mauritsseelen@hotmail.com>
2023-03-07 14:01:27 +0100

    implement more unit tests

--- Chatterz.API.Tests/ChatroomControllerTests.cs
+++ Chatterz.API.Tests/ChatroomControllerTests.cs
@@ -14,14 +14,14 @@ public class ChatroomControllerTests
     private readonly Mock<IUserService> _userServiceMock;
     private readonly Mock<ISignalRManager> _signalRManager;
     private readonly Mock<IChatroomService> _chatroomServiceMock;
-    private readonly Mock<IService<ChatMessage>> _chatMessageServiceMock;
+    private readonly Mock<IChatMessageService> _chatMessageServiceMock;
     private readonly ChatroomController _chatroomController;
 
     public ChatroomControllerTests()
     {
         _userServiceMock = new Mock<IUserService>();
         _signalRManager = new Mock<ISignalRManager>();
-        _chatMessageServiceMock = new Mock<IService<ChatMessage>>();
+        _chatMessageServiceMock = new Mock<IChatMessageService>();
         _chatroomServiceMock = new Mock<IChatroomService>();
         _chatroomController = new ChatroomController(
             _userServiceMock.Object,
@@ -45,7 +45,7 @@ public class ChatroomControllerTests
 
         // assert
         Assert.IsNotNull(result);
-        Assert.IsInstanceOfType(result, typeof(ActionResult));
+        Assert.IsInstanceOfType(result, typeof(OkResult));
     }
 
     [TestMethod]
@@ -63,11 +63,11 @@ public class ChatroomControllerTests
 
         // assert
         Assert.IsNotNull(result);
-        Assert.IsInstanceOfType(result, typeof(ActionResult));
+        Assert.IsInstanceOfType(result, typeof(OkResult));
     }
 
     [TestMethod]
-    public async Task Create_Chatroom_Returns_Ok_With_Generated_Id()
+    public async Task Create_Chatroom_Returns_Ok_With_Generated_Id_Calls_Service_GetAllWithUsers_AddChatroomAsync()
     {
         // arrange
 
@@ -75,8 +75,54 @@ public class ChatroomControllerTests
         var result = await _chatroomController.Create();
 
         // assert
+        _chatroomServiceMock.Verify(s => s.GetAllWithUsers(), Times.Once);
+        _chatroomServiceMock.Verify(s => s.AddChatroomAsync(), Times.Once);
         Assert.IsNotNull(result);
         Assert.IsInstanceOfType(result, typeof(ActionResult<int>));
+    }
+
+    [TestMethod]
+    public async Task Remove_Chatroom_Returns_Ok_Calls_Service_RemoveChatroom()
+    {
+        // arrange
+        var chatroomId = 1;
+
+        // act
+        var result = await _chatroomController.Remove(1);
+
+        // assert
+        _chatroomServiceMock.Verify(s => s.RemoveChatroom(chatroomId), Times.Once);
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(OkResult));
+    }
+
+    [TestMethod]
+    public async Task GetAll_Returns_Ok_Calls_Service_GetAllWithUsers()
+    {
+        // arrange
+
+        // act
+        var result = await _chatroomController.GetAll();
+
+        // assert
+        _chatroomServiceMock.Verify(s => s.GetAllWithUsers(), Times.Once);
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(ActionResult<List<Chatroom>>));
+    }
+
+    [TestMethod]
+    public async Task GetChatHistory_Returns_Ok_Calls_Service_GetChatHistory()
+    {
+        // arrange
+        var chatroomId = 1;
+
+        // act
+        var result = await _chatroomController.GetChatHistory(chatroomId);
+
+        // assert
+        _chatMessageServiceMock.Verify(s => s.GetChatHistory(chatroomId), Times.Once);
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(ActionResult<List<ChatMessage>>));
     }
 
     private ChatroomJoinDto GetChatroomDto(string connectionId)
